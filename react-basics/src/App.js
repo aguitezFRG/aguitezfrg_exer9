6b2ebfa78fb86988e41ba0ee068a2cd69952eb8f
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-06 17:20:22 +0800

    Revert "feat/refactor: modified contents to fit specifications, implemented item removal"
    This reverts commit 4a3a3b183f5f144d4ad9f57e67011f394cf4883c.

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,13 +1,9 @@
 import './App.css'; // import stylesheet
-import Menu from './components/menu';  // import array to be used for the "menu"
-import Item from './components/items'; // import array of items to be shown
-import Cart from './components/cart';   // import array of items in cart
-
-import { useState } from 'react';
+import menus from './components/menu';  // import array to be used for the "menu"
+import items from './components/items'; // import array of items to be shown
+import cart from './components/cart';   // import array of items in cart
 
 function App() {
-  const [cart, setCart] = useState([])
-
   return (
     <div className="App">
       {/* navbar containing the app name and the different "menu" */}
@@ -15,96 +11,70 @@ function App() {
         <div className="Lazado">
           <p>Lazado</p>
         </div>
-        <Menu list={menus} />
+        <div className="select">
+          {menus.map(menu => ( // mapping elements of the menus array
+            <h3 key={menu.id} id={menu.name.toLowerCase()}> 
+              {menu.name}
+            </h3>
+          ))}
+        </div>
       </div>
       <div className="App_body">
-        <Item list={items} setCart={setCart} cart={cart}/>
-        <Cart list={cart} setCart={setCart}  cart={cart}/>
+        {/* shows the elements of the items array */}
+        <div className="items">
+          {items.map(item=> ( // mapping of the individual elements
+            <div key={item.id} className="item">
+              <div className="item_details">
+                {/* order is reversed */}
+                {/* adds a button to the bottom part of the item details  */}
+                <button className="addtocart" onClick={() => handleAddToCart(item)}>Add to Cart</button>
+                <p id="item_price">${item.price}</p>
+                <p id="item_name">{item.name}</p>
+                <img className="item_img" src={item.image} alt={item.name}/>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="cart">
+          <div className="cart_header">
+            <p id="cart_title">Shopping Cart</p>
+            <p id="num_items">Item Count: {cart.length}</p>
+          </div>
+          <div className="cart_items">
+            {cart.map(cart_item => (
+              <div key = {cart_item.id} className="cart_item">
+                  <button className="removeItem">Remove</button>
+                  <div id="cart_item_price">${computeTotal(cart_item)}</div>
+                  <div id="cart_item_quantity">QTY: {cart_item.quantity}</div>
+                  <div id="cart_item_name">{cart_item.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-const menus = [
-  { name: "Appliances", url: "#appliance", id: 1},
-  { name: "Groceries", url: "#grocery", id: 2},
-  { name: "Gadgets", url: "#gadget", id: 3}
-];
+// function logs to the console an item added to the cart
+function handleAddToCart(item) {
+  console.log(`Added ${item.name} to cart.`);
+}
 
-const items = [
-  {
-      id: 1,
-      name: 'iPhone',
-      price: 749,
-      image: 'https://img.freepik.com/free-photo/front-view-hand-holding-smartphone_23-2148775905.jpg?w=360&t=st=1714897773~exp=1714898373~hmac=7b8a20fe9c24cc72c3e6a71f38fe20ffe07ef3d0f6c8c6890278e82568aec7a9'
-  },
-  {
-      id: 2,
-      name: 'Laptop',
-      price: 699,
-      image: 'https://img.freepik.com/free-photo/laptop-pencils-arrangement_23-2148128294.jpg?t=st=1714918664~exp=1714922264~hmac=62e6facb7a2f7891f4d478a400144f186e91e8530c9040ae9555534a55ab604f&w=900'
-  },
-  {
-      id: 3,
-      name: 'Earphones',
-      price: 199,
-      image: 'https://img.freepik.com/free-vector/headphones-wireless-realistic-composition-with-isolated-image-phones-with-power-bank-dock-station-with-reflections-vector-illustration_1284-73201.jpg?t=st=1714918934~exp=1714922534~hmac=834dabc524b56c434ca78b7606ed8f282cd15f0c9fe9719e9ffb1d3bd321a8e4&w=740'
-  },
-  {
-      id: 4,
-      name: 'Headset',
-      price: 249,
-      image: 'https://img.freepik.com/free-photo/levitating-music-headphones-display_23-2149817602.jpg?t=st=1714918994~exp=1714922594~hmac=27d3658b8a41dedaaf584c6b1be77b3b8ead9f2fb11052175c634812fa702a53&w=360'
-  },
-  {
-      id: 5,
-      name: 'Speaker',
-      price: 229,
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBXHSLbVTmDUTvTpqW1kUbV5UerVJB4mqdAtVLgby8Jw&s'
-  },
-  {
-      id: 6,
-      name: 'Computer',
-      price: 499,
-      image: 'https://cdn.originpc.com/img/compare-all/gaming-desktops/genesis-7000-series-system-image.png'
-  },
-  {
-      id: 7,
-      name: 'Monitor',
-      price: 549,
-      image: 'https://cdn.arstechnica.net/wp-content/uploads/2022/01/XXXXXX.jpg'
-  },
-  {
-      id: 8,
-      name: 'Samsung',
-      price: 699,
-      image: 'https://images.samsung.com/is/image/samsung/p6pim/ph/2401/gallery/ph-galaxy-s24-s928-sm-s928bztqphl-thumb-539303751'
-  },
-  {
-      id: 9,
-      name: 'Aircon',
-      price: 549,
-      image: 'https://www.lg.com/my/images/home-air-conditioners/bs-q126bry4/gallery/Artcool_zoom_02.jpg'
-  },
-  {
-      id: 10,
-      name: 'EFan',
-      price: 149,
-      image: 'https://ansons.ph/wp-content/uploads/2022/05/01_BKWST-16BK.png'
-  },
-  {
-      id: 11,
-      name: 'Air Cooler',
-      price: 249,
-      image: 'https://d1pjg4o0tbonat.cloudfront.net/content/dam/midea-aem/id/AC120-16AR-BLUE-LIGHT.png/jcr:content/renditions/cq5dam.compression.png'
-  },
-  {
-      id: 12,
-      name: 'Washing Machine',
-      price: 349,
-      image: 'https://www.lg.com/ph/images/washing-machines/md07556668/gallery1/D-2.jpg'
-  }
-  
-];
+function computeTotal(item) {
+  var total = item.quantity * item.price;
+  return total;
+}
+
+document.querySelectorAll('.removeItem').forEach((button) => {
+  button.addEventListener('mouseover', function() {
+    this.parentNode.style.backgroundColor = '#FF6663'; // replace 'color' with your desired color
+    this.parentNode.style.color = 'white';
+  });
+  button.addEventListener('mouseout', function() {
+    this.parentNode.style.backgroundColor = ''; // resets the color when the mouse is no longer hovering over the button
+    this.parentNode.style.color = '';
+  });
+});
 
 export default App;
--- react-basics/src/components/cart.js
+++ react-basics/src/components/cart.js
@@ -1,49 +1,17 @@
-import React from "react";
-
-export default function CartList(props) {
-    const cart = props.list;
-    const [isHovered, setIsHovered] = React.useState(false);
-
-    const computeTotal = (item) => {
-        var total = item.quantity * item.price;
-        return total;
+const cart_items = [
+    {
+        id: 1,
+        name: 'Earphones',
+        quantity: 2,
+        price: 199
+    },
+    {
+        id: 2,
+        name: 'iPhone',
+        quantity: 2,
+        price: 749
     }
 
-    const handleRemoveItem = (item) => {
-        props.setCart((curr_cart) => {
-            const new_cart = curr_cart.filter((cart_item) => cart_item.id !== item.id)   
-        console.log("Successfully removed " + item.name + " from the cart");
-        return new_cart;
-        });
-    }
+]
 
-    return (
-        <div className="cart">
-            <div className="cart_header">
-                <p id="cart_title">Shopping Cart</p>
-                <p id="num_items">Item Count: {cart.length}</p>
-            </div>
-            {cart.length === 0 ? (
-                <div className="emptyCart">
-                    Cart is currently empty.
-                </div>
-            ) : (
-                <div className="cart_items">
-                    {cart.map(cart_item => (
-                    <div key={cart_item.id} className={`cart_item ${isHovered === cart_item.id ? 'hovered' : ''}`}>
-                        <button className="removeItem" 
-                            onClick={() => handleRemoveItem(cart_item)} 
-                            onMouseEnter={() => setIsHovered(cart_item.id)} 
-                            onMouseLeave={() => setIsHovered(null)}>
-                                Remove
-                        </button>
-                        <div id="cart_item_price">${computeTotal(cart_item)}</div>
-                        <div id="cart_item_quantity">QTY: {cart_item.quantity}</div>
-                        <div id="cart_item_name">{cart_item.name}</div>
-                    </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    )
-};
+export default cart_items;
