6249f7c1b3dac0c06abacfd8f67fcf1c76fdce46
aguitezFRG <fpaguitez@up.edu.ph>
2024-05-06 11:42:26 +0800

    feat/test: added barebones algorithm for handling the shopping cart section of the app

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'; // import stylesheet
-import menus from './components/menu'; // import array to be used for the "menu"
-import items from './components/items' // import array of items to be shown
+import menus from './components/menu';  // import array to be used for the "menu"
+import items from './components/items'; // import array of items to be shown
+import cart from './components/cart';   // import array of items in cart
 
 function App() {
   return (
@@ -18,20 +19,40 @@ function App() {
           ))}
         </div>
       </div>
-      {/* shows the elements of the items array */}
-      <div className="items">
-        {items.map(item=> ( // mapping of the individual elements
-          <div key={item.id} className="item">
-            <div className="item_details">
-              {/* order is reversed */}
-              {/* adds a button to the bottom part of the item details  */}
-              <button className="addtocart" onClick={() => handleAddToCart(item)}>Add to Cart</button>
-              <p id="item_price">${item.price}</p>
-              <p id="item_name">{item.name}</p>
-              <img className="item_img" src={item.image} alt={item.name}/>
+      <div className="App_body">
+        {/* shows the elements of the items array */}
+        <div className="items">
+          {items.map(item=> ( // mapping of the individual elements
+            <div key={item.id} className="item">
+              <div className="item_details">
+                {/* order is reversed */}
+                {/* adds a button to the bottom part of the item details  */}
+                <button className="addtocart" onClick={() => handleAddToCart(item)}>Add to Cart</button>
+                <p id="item_price">${item.price}</p>
+                <p id="item_name">{item.name}</p>
+                <img className="item_img" src={item.image} alt={item.name}/>
+              </div>
             </div>
+          ))}
+        </div>
+        <div className="cart">
+          <div className="cart_header">
+            <p className="cart_title">Shopping Cart</p>
+            <p className="num_items">{cart.length}</p>
           </div>
-        ))}
+          <div className="cart_items">
+            {cart.map(cart_item => (
+              <div key = {cart_item.id} className="cart_item">
+                <div className="cart_item_details">
+                    <div id="cart_item_name">{cart_item.name}</div>
+                    <div id="cart_item_quantity">{cart_item.quantity}</div>
+                    <div id="cart_item_price">{computeTotal(cart_item)}</div>
+                    <button className="removeItem">Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -39,7 +60,12 @@ function App() {
 
 // function logs to the console an item added to the cart
 function handleAddToCart(item) {
-    console.log(`Added ${item.name} to cart.`);
+  console.log(`Added ${item.name} to cart.`);
+}
+
+function computeTotal(item) {
+  var total = item.quantity * item.price;
+  return total;
 }
 
 export default App;
---
+++ react-basics/src/components/cart.js
@@ -0,0 +1,11 @@
+const cart_items = [
+    {
+        id: 1,
+        name: 'Earphones',
+        quantity: 2,
+        price: 199
+    }
+
+]
+
+export default cart_items;
